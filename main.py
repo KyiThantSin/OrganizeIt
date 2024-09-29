ae7b801acd4864a06482bad6620de7d5fccabe63
KyiThantSin <kyits485@gmail.com>
2024-09-29 10:39:33 +0700

    task-card-ui

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ class TaskManagementSystem:
         self.root = root  # ctk 
         self.root.title("OrganizeIt")
         self.root.geometry("900x700")
-
+            
         # padding
         self.padx = 80
         
         # font 
         self.custom_title_font = ctk.CTkFont(family="Arial", size=24, weight="bold")
         self.custom_label_font = ctk.CTkFont(family="Arial", size=16, weight="normal")
+        self.custom_task_label_font = ctk.CTkFont(family="Arial", size=14, weight="bold")
 
         # app title 
         self.title_label = ctk.CTkLabel(self.root, text="OrganizeIt", font=self.custom_title_font)
@@ -27,6 +28,7 @@ class TaskManagementSystem:
         self.filter_status_var = ctk.StringVar()
 
         self.create_filter_section()
+        self.create_task_card()
     
     def create_filter_section(self):
         filter_frame = ctk.CTkFrame(self.root, corner_radius=10)  # Child of main window
@@ -56,6 +58,19 @@ class TaskManagementSystem:
         filter_frame.grid_columnconfigure(2, weight=0)  
         filter_frame.grid_columnconfigure(3, weight=0) 
 
+         # task list frame
+        self.tasks_list_frame = ctk.CTkFrame(self.root, corner_radius=10, fg_color="transparent")
+        self.tasks_list_frame.pack(padx=self.padx, pady=10, fill="both", expand = True)
+
+    def create_task_card(self):
+        card_frame = ctk.CTkFrame(self.tasks_list_frame, corner_radius=10)
+        card_frame.pack(padx=0, pady=(10,10), fill="x")
+
+        ctk.CTkLabel(card_frame, text=f"Task", font=self.custom_task_label_font).grid(row=0, column=0, sticky="w", padx=20, pady=(10,5))
+        ctk.CTkLabel(card_frame, text=f"Description:").grid(row=1, column=0, sticky="w", padx=20)
+        ctk.CTkLabel(card_frame, text=f"Tag: ").grid(row=2, column=0, sticky="w", padx=20)
+        ctk.CTkLabel(card_frame, text=f"Status: ").grid(row=3, column=0, sticky="w", padx=20)
+        ctk.CTkLabel(card_frame, text=f"Date: ").grid(row=4, column=0, sticky="w", padx=20, pady=(5,10))
 
 def main():
     root = ctk.CTk()
